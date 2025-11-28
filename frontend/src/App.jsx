1852153682ec5a6897db32254cff9005ba659e29
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2025-11-28 14:39:43 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Navbar/Navbar";
+const App = () => {
+	return (
+		<div>
+			<h1>Jay ganesh</h1>
+			<Navbar />
+		</div>
+	);
+};
+
+export default App;
+
